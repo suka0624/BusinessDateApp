11c41ebf7bb500e5ee6fb07882eecfd9a90a42e0
Shintaro Suka <suka0624suka@gmail.com>
2019-10-14 21:52:03 +0900

    RepositoryのUnitテスト追加

--- src/main/java/com/suka/springboot/domain/BusinessDate.java
+++ src/main/java/com/suka/springboot/domain/BusinessDate.java
@@ -2,7 +2,6 @@ package com.suka.springboot.domain;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 /**
