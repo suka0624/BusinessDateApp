4efdbe3adc02240b7be017918ee38582ccde1620
Shintaro Suka <suka0624suka@gmail.com>
2019-10-16 17:17:25 +0900

    selenideによるUIテスト項目追加(一覧表示＆登録)

--- src/test/java/com/suka/springboot/selenide/BusinessDateSelenideTest.java
+++ src/test/java/com/suka/springboot/selenide/BusinessDateSelenideTest.java
@@ -10,6 +10,9 @@ import com.suka.springboot.BusinessDateAppApplication;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * 業務日付管理画面テスト
  * 
@@ -44,12 +47,53 @@ public class BusinessDateSelenideTest {
         WebDriverRunner.closeWebDriver();
     }
     
+    /**
+     * 一覧画面の表示内容を確認する
+     * @throws Exception
+     */
     @Test
     public void 一覧画面表示時のUI確認() throws Exception {
     	
     	ListPage.一覧画面を表示();
     	
+    	/** 画面タイトルが”業務日付管理”であることをテスト **/
     	assertThat(ListPage.タイトル()).isEqualTo("業務日付管理");
     	
+    	/** デフォルトの基準日が現在日であることをテスト **/
+    	assertThat(ListPage.基準日()).isEqualTo(getCurrent());
+    }
+    
+    /**
+     * 業務日付用計算式を新規登録する
+     * @throws Exception
+     */
+    @Test
+    public void 業務日付用計算式() throws Exception {
+    	
+    	ListPage.一覧画面を表示();
+    	
+    	/** 登録前の件数確認 **/
+    	assertThat(ListPage.登録件数()).isEqualTo(3);
+    	
+    	/** 業務日付用計算式の新規登録 **/
+    	ListPage.業務日付名は("3ヶ月後");
+    	ListPage.加減年は("0");
+    	ListPage.加減月は("3");
+    	ListPage.加減日は("0");
+    	ListPage.で登録する();
+    	
+    	/** 登録後の件数確認 **/
+    	assertThat(ListPage.登録件数()).isEqualTo(4);    	
+    }
+    
+    /**
+     * 現在日をYYYY/MM/DD形式で返却する
+     * 
+     * @return 現在日(YYYY/MM/DD形式)
+     */
+    private String getCurrent() {
+    	LocalDateTime now = LocalDateTime.now();
+    	DateTimeFormatter dfNow = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    	return now.format(dfNow);
     }
 }
--- src/test/java/com/suka/springboot/selenide/ListPage.java
+++ src/test/java/com/suka/springboot/selenide/ListPage.java
@@ -1,5 +1,7 @@
 package com.suka.springboot.selenide;
 
+import org.openqa.selenium.By;
+
 import com.codeborne.selenide.Selenide;
 
 /**
@@ -10,7 +12,7 @@ import com.codeborne.selenide.Selenide;
 public class ListPage {
 
 	/** 一覧(/登録)画面URL **/
-	private static final String URL = "http://localhost:8080";
+	private static final String URL = "http://localhost:8888";
 	
 	/**
 	 * 一覧(/登録)画面を開く
@@ -25,5 +27,67 @@ public class ListPage {
 	 */
     public static String タイトル(){
         return Selenide.title();
+    }
+    
+    /**
+     * 基準日を取得する
+     * 
+     * @return 基準日
+     */
+    public static String 基準日() {
+    	return Selenide.$(By.id("base")).getValue();	
+    }
+    
+    /**
+     * 登録件数を取得する
+     * 
+     * @return 登録件数
+     */
+    public static int 登録件数(){
+    	// 最後の「-1」はヘッダ分
+        return ( Selenide.$(By.name("formList")).$("table").$$("tr").size() - 1);
+    }
+    
+    /**
+     * 登録用に業務日付名を設定する
+     * 
+     * @param name 業務日付名
+     */
+    public static void 業務日付名は(String name) {
+        Selenide.$(By.name("entryName")).setValue(name);
+    }
+    
+    /**
+     * 登録用に加減年を設定する
+     * 
+     * @param year 加減年
+     */
+    public static void 加減年は(String year) {
+    	Selenide.$(By.name("entryYear")).setValue(year);
+    }
+    
+    /**
+     * 登録用に加減月を設定する
+     * 
+     * @param month 加減月
+     */
+    public static void 加減月は(String month) {
+    	Selenide.$(By.name("entryMonth")).setValue(month);
+    }
+    
+    /**
+     * 登録用に加減日を設定する
+     * 
+     * @param day 加減日
+     */
+    public static void 加減日は(String day) {
+    	Selenide.$(By.name("entryDay")).setValue(day);
+    }
+    
+    /**
+     * 業務日付計算用情報を新規に登録する
+     */
+    public static void で登録する(){
+        Selenide.$(By.id("entryBtn")).click();
     }
 }
