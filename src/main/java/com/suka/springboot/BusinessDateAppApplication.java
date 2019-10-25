6599d75ba31dcf654dc72831ebd2cdbe24530ee1
Shintaro Suka <suka0624suka@gmail.com>
2019-10-25 12:41:36 +0900

    サーバ上のTomcatで動作するようにFormのAction記載を修正

--- src/main/java/com/suka/springboot/BusinessDateAppApplication.java
+++ src/main/java/com/suka/springboot/BusinessDateAppApplication.java
@@ -2,12 +2,19 @@ package com.suka.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BusinessDateAppApplication {
+public class BusinessDateAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessDateAppApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BusinessDateAppApplication.class);
+    }
 
 }
